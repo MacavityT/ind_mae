664409ee2f22b52eb0196844779355dd542ba0e6
Macavity <taiyan2020@ia.ac.cn>
2022-04-22 17:14:07 +0800

    1

--- ind_utils/check_file.py
+++ ind_utils/check_file.py
@@ -1,11 +1,15 @@
 import os
 import os.path as osp
+from tqdm import trange
 
 root = '/mnt/VMSTORE/IndDatasets'
 ann_file = osp.join(root, 'train.txt')
 with open(ann_file, 'r') as f:
     samples = [osp.join(root, x.strip()) for x in f.readlines()]
-
+bar = trange(len(samples))
 for name in samples:
-    if not os.path.exists(name):
-        print(name)
+    if not os.access(name, os.F_OK):
+        print(f'Exists error: {name}')
+    if not os.access(name, os.R_OK):
+        print(f'Access error: {name}')
+    bar.update()
