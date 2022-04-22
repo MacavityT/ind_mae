1cf7784afa4da366d270edfc9f1dbdb3a821eebd
Macavity <taiyan2020@ia.ac.cn>
2022-04-22 17:05:15 +0800

    1

---
+++ ind_utils/check_file.py
@@ -0,0 +1,11 @@
+import os
+import os.path as osp
+
+root = '/mnt/VMSTORE/IndDatasets/train.txt'
+ann_file = osp.join(root, 'train.txt')
+with open(ann_file, 'r') as f:
+    samples = [osp.join(root, x.strip()) for x in f.readlines()]
+
+for name in samples:
+    if not os.path.exists(name):
+        print(name)
