151bbf2af50b664cfd3e4f8ccacbf294e7b1a88e
Macavity <taiyan2020@ia.ac.cn>
2022-04-23 08:47:52 +0800

    modified shared folder

--- submitit_pretrain.py
+++ submitit_pretrain.py
@@ -53,8 +53,12 @@ def parse_args():
 
 def get_shared_folder() -> Path:
     user = os.getenv("USER")
-    if Path("/checkpoint/").is_dir():
-        p = Path(f"/checkpoint/{user}/experiments")
+    # if Path("/checkpoint/").is_dir():
+    #     p = Path(f"/checkpoint/{user}/experiments")
+    #     p.mkdir(exist_ok=True)
+    #     return p
+    if Path("/mnt/VMSTORE/workspace_ty/").is_dir():
+        p = Path(f"/mnt/VMSTORE/workspace_ty/{user}/experiments")
         p.mkdir(exist_ok=True)
         return p
     raise RuntimeError("No shared folder available")
