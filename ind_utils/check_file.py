ab9f4fd7cc7e0b88fcc849a97bf21d39a593124f
Macavity <taiyan2020@ia.ac.cn>
2022-04-23 16:08:13 +0800

    1

--- ind_utils/check_file.py
+++ ind_utils/check_file.py
@@ -2,7 +2,7 @@ import os
 import os.path as osp
 from tqdm import trange
 
-root = '/mnt/VMSTORE/IndDatasets'
+root = '/mnt/VMSTORE/workspace_ty/IndDatasets'
 ann_file = osp.join(root, 'train.txt')
 with open(ann_file, 'r') as f:
     samples = [osp.join(root, x.strip()) for x in f.readlines()]
