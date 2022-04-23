000c259bfad68b3a9997d9346dd1181281b56ff4
Macavity <taiyan2020@ia.ac.cn>
2022-04-23 16:13:56 +0800

    1

--- ind_utils/check_file.py
+++ ind_utils/check_file.py
@@ -7,7 +7,12 @@ ann_file = osp.join(root, 'train.txt')
 with open(ann_file, 'r') as f:
     samples = [osp.join(root, x.strip()) for x in f.readlines()]
 bar = trange(len(samples))
+count = 0
 for name in samples:
+    count += 1
+    if count > 100:
+        break
+
     if not os.access(name, os.F_OK):
         print(f'Exists error: {name}')
     if not os.access(name, os.R_OK):
