cb91d0aa13d9de1e4dfb7b76a083ccec6a026ea2
root <root@localhost.localdomain>
2022-04-27 17:32:53 +0800

    add run bash

---
+++ ind_runfiles/0422_pretrain.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# export CUDA_VISIBLE_DEVICES=0,1,2,3
+# export CUDA_VISIBLE_DEVICES=4,5,6,7
+GPUS=6
+PORT=${PORT:-29500}
+NAME=0422_pretrain
+
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+    $(dirname "$0")/../main_pretrain.py \
+    --output_dir $(dirname "$0")/../ind_models/$NAME \
+    --log_dir $(dirname "$0")/../ind_models/$NAME \
+    --batch_size 24 \
+    --accum_iter 28 \
+    --model mae_vit_base_patch16 \
+    --norm_pix_loss \
+    --mask_ratio 0.75 \
+    --epochs 200 \
+    --warmup_epochs 40 \
+    --blr 1.5e-4 --weight_decay 0.05 \
+    --data_path $(dirname "$0")/../ind_data/ \
+    --local_rank 0 \
+    --resume $(dirname "$0")/../ind_models/$NAME/checkpoint-80.pth
