9b56ef4e12c2156a5dde4ba70acbe83f377a7e18
Macavity <taiyan2020@ia.ac.cn>
2022-04-27 20:47:52 +0800

    add prob sh

--- ind_runfiles/0427_finetune0_imgnet.sh
+++ ind_runfiles/0427_finetune0_imgnet.sh
@@ -17,7 +17,6 @@ python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     --input_size 224 \
     --model vit_base_patch16 \
     --epochs 100 \
-    --warmup_epochs 40 \
     --blr 5e-4 --layer_decay 0.65 \
     --weight_decay 0.05 --drop_path 0.1 --mixup 0.8 --cutmix 1.0 --reprob 0.25 \
     --dist_eval \
