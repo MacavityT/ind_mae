c74e8190cd682802e0ccf7d4ff79352c08e6ecad
root <root@localhost.localdomain>
2022-04-27 18:21:07 +0800

    add runfiles

--- ind_runfiles/0427_finetune0_ind.sh
+++ ind_runfiles/0427_finetune0_ind.sh
@@ -3,15 +3,15 @@
 # export CUDA_VISIBLE_DEVICES=4,5,6,7
 GPUS=6
 PORT=${PORT:-29500}
-NAME=finetune
+NAME=0427_finetune0_ind
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     $(dirname "$0")/../main_finetune.py \
     --output_dir $(dirname "$0")/../ind_models/$NAME \
     --log_dir $(dirname "$0")/../ind_models/$NAME \
-    --finetune ${PRETRAIN_CHKPT} \
-    --nb_classes 5 \
+    --finetune $(dirname "$0")/../ind_models/0422_pretrain/checkpoint-199.pth \
+    --nb_classes 2 \
     --accum_iter 7 \
     --batch_size 24 \
     --input_size 224 \
@@ -20,6 +20,7 @@ python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     --warmup_epochs 40 \
     --blr 5e-4 --layer_decay 0.65 \
     --weight_decay 0.05 --drop_path 0.1 --mixup 0.8 --cutmix 1.0 --reprob 0.25 \
-    --dist_eval --data_path $(dirname "$0")/../ind_data/ \
+    --dist_eval \
+    --img_prefix --data_path $(dirname "$0")/../ind_data/Bridge_Crack_Image/DBCC_Training_Data_Set \
     --local_rank 0
     # --resume $(dirname "$0")/../ind_models/$NAME/checkpoint-80.pth
