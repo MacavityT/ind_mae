b13bce3978b536b197e66b6a756ef1653a80a301
root <root@localhost.localdomain>
2022-04-27 19:04:18 +0800

    add runfiles

---
+++ ind_runfiles/0427_finetune1_ind.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# export CUDA_VISIBLE_DEVICES=0,1,2,3
+# export CUDA_VISIBLE_DEVICES=4,5,6,7
+GPUS=6
+PORT=${PORT:-29500}
+NAME=0427_finetune1_ind
+
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+    $(dirname "$0")/../main_finetune.py \
+    --output_dir $(dirname "$0")/../ind_models/$NAME \
+    --log_dir $(dirname "$0")/../ind_models/$NAME \
+    --finetune $(dirname "$0")/../ind_models/0422_pretrain/checkpoint-199.pth \
+    --nb_classes 28 \
+    --accum_iter 7 \
+    --batch_size 24 \
+    --input_size 224 \
+    --model vit_base_patch16 \
+    --epochs 100 \
+    --warmup_epochs 40 \
+    --blr 5e-4 --layer_decay 0.65 \
+    --weight_decay 0.05 --drop_path 0.1 --mixup 0.8 --cutmix 1.0 --reprob 0.25 \
+    --dist_eval \
+    --data_path $(dirname "$0")/../ind_data/Kylberg_Texture_Dataset_v.1.0 \
+    --local_rank 0
+    # --resume $(dirname "$0")/../ind_models/$NAME/checkpoint-80.pth
