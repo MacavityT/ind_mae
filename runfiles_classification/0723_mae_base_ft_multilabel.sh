fc8581d6db6d7641ed2d7fd888524b9ce1d2485e
Macavity <taiyan2020@ia.ac.cn>
2022-07-23 20:45:04 +0800

    modefied to fit cluster

--- runfiles_classification/0723_mae_base_ft_multilabel.sh
+++ runfiles_classification/0723_mae_base_ft_multilabel.sh
@@ -10,7 +10,7 @@ python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     $(dirname "$0")/../main_finetune_sewer.py \
     --output_dir $(dirname "$0")/../ind_models/$NAME \
     --log_dir $(dirname "$0")/../ind_models/$NAME \
-    --finetune /home/taiyan/models/pretrain/mae/checkpoint-199.pth \
+    --finetune $(dirname "$0")/../ind_models/mae_imagenet1k/mae_pretrain_vit_base.pth \
     --ds_mode e2e \
     --nb_classes 17 \
     --accum_iter 7 \
@@ -23,4 +23,3 @@ python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     --dist_eval \
     --data_path $(dirname "$0")/../ind_data/SewerML
     # --resume $(dirname "$0")/../ind_models/$NAME/checkpoint-80.pth
-    # --finetune $(dirname "$0")/../ind_models/mae_imagenet1k/mae_pretrain_vit_base.pth \
